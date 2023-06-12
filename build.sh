50d96e094555af8d1f453e40ccb02cb915eaa295
Zhan Rongrui <2742392377@qq.com>
2023-06-12 04:39:47 +0000

    add test build ci

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 mkdir build && cd build
 cmake -G Ninja .. \
+    -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
+    -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DCMAKE_BUILD_TYPE=RELEASE
 ninja install
